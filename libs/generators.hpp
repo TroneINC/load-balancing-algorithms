e5e3ddd6147643b193a89eea59a1d76920af157c
X_xp <timurbilalov14@gmail.com>
2026-06-11 16:35:22 +0300

    add generators

---
+++ libs/generators.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <numbers>
+#include <optional>
+#include <random>
+#include <span>
+namespace load_balancer {
+struct IGenerator {
+    virtual double next(std::mt19937& rnd) = 0;
+    virtual ~IGenerator() = default;
+};
+
+struct SequenceGenerator : IGenerator {
+   private:
+    std::vector<double> seq_;
+    size_t pos_;
+
+   public:
+    SequenceGenerator(std::span<double> seq) {
+        pos_ = 0;
+        seq_.reserve(seq.size());
+        for (size_t index = 0; index < seq.size(); index++) {
+            seq_[index] = seq[index];
+        }
+    }
+    double next(std::mt19937&) {
+        if (pos_ > seq_.size()) {
+            pos_ -= seq_.size();
+        }
+        return seq_[pos_++];
+    }
+};
+
+class UniformGenerator : IGenerator {
+   public:
+    UniformGenerator(double min, double max)
+        : dist_(min, max) {}
+    double next(std::mt19937& rng) override {
+        return dist_(rng);
+    }
+
+   private:
+    std::uniform_real_distribution<double> dist_;
+};
+
+class NormalGenerator : IGenerator {
+   public:
+    NormalGenerator(double center, double deviation, std::optional<double> min = {}, std::optional<double> max = {})
+        : dist_(center, deviation)
+        , min_(min)
+        , max_(max) {}
+
+    double next(std::mt19937& rng) override {
+        double val = dist_(rng);
+        if (min_) {
+            val = std::max(*min_, val);
+        }
+        if (max_) {
+            val = std::min(*max_, val);
+        }
+        return val;
+    }
+
+   private:
+    std::normal_distribution<double> dist_;
+    std::optional<double> min_, max_;
+};
+
+class ExponentialGenerator : IGenerator {
+   public:
+    explicit ExponentialGenerator(double mean)
+        : dist_(1.0 / mean) {}
+    double next(std::mt19937& rng) override {
+        return dist_(rng);
+    }
+
+   private:
+    std::exponential_distribution<double> dist_;
+};
+
+class LognormalGenerator : IGenerator {
+   public:
+    LognormalGenerator(double center, double deviation, std::optional<double> min = {}, std::optional<double> max = {})
+        : dist_(center, deviation)
+        , min_(min)
+        , max_(max) {}
+    double next(std::mt19937& rng) override {
+        double val = dist_(rng);
+        if (min_) {
+            val = std::max(*min_, val);
+        }
+        if (max_) {
+            val = std::min(*max_, val);
+        }
+        return val;
+    }
+
+   private:
+    std::lognormal_distribution<double> dist_;
+    std::optional<double> min_, max_;
+};
+
+}  // namespace load_balancer
